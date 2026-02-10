444ed517283501e02ebdbaff1fa76750951bffc4
Jan Philipp Gnau <janphilipp.gnau@alumni.esade.edu>
2026-02-10 13:57:30 +0100

    Fix envelope rendering

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ st.markdown('<div class="sub"></div>', unsafe_allow_html=True)
 st.markdown(
     """
 <div class="card fade-in">
-  <div class="lead">Pauli mein Schatz, ich frage mich das wirklich oft…</div>
+  <div class="lead">Pauli mein Schatz, </div>
 
   <p>
     Wie kann ein Mensch das Leben so sehr verändern – nicht laut, nicht auf einmal,
