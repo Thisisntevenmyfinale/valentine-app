ed7665e446b6797878a823b1d4ec9f07e22ab53a
Jan Philipp Gnau <janphilipp.gnau@alumni.esade.edu>
2026-02-10 16:43:27 +0100

    ...

--- app.py
+++ app.py
@@ -371,7 +371,7 @@ mobile_css = """
 .b1{ left: 65%; top: 190px; transform: translateX(-50%); }
 
 /* “Desssert” between Sat & Sun (night) */
-.b2{ left: 40%; top: 400px; transform: translateX(-50%); }
+.b2{ left: 40%; top: 420px; transform: translateX(-50%); }
 
 /* Curve: keep it similar but ensure it stays inside + not too thick */
 .curve{
